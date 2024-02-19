cbdc5fc4d04b59b0d21753a30f2367098d0e0042
HaDock404 <delescluse.g@gmail.com>
2024-02-19 11:53:50 +0100

    advanced model

--- production/api/main.py
+++ production/api/main.py
@@ -4,6 +4,7 @@ from flask import request
 import joblib
 from cleaning import preprocess_text
 import os
+import tensorflow as tf
 
 app = Flask(__name__)
 
@@ -17,16 +18,20 @@ def home():
 def predict_sentence():
     user_input = request.form['X_test']
 
-    vectorizer_path = os.path.join("production/api/models",
-                                   "vectorizer.pkl")
+    # vectorizer_path = os.path.join("production/api/models",
+    #                               "vectorizer.pkl")
 
-    with open(vectorizer_path, 'rb') as vec_file:
-        vectorizer = joblib.load(vec_file)
+    # with open(vectorizer_path, 'rb') as vec_file:
+    #    vectorizer = joblib.load(vec_file)
+
+    saved_model_path = "production/api/models"
+    embedding = tf.saved_model.load(saved_model_path)
 
     preprocess_sentence = user_input
 
     preprocess_sentence = preprocess_text(preprocess_sentence)
-    preprocess_sentence = vectorizer.transform([preprocess_sentence])
+    # preprocess_sentence = vectorizer.transform([preprocess_sentence])
+    preprocess_sentence = embedding([preprocess_sentence])
 
     model_path = os.path.join("production/api/models",
                               "model.pkl")
@@ -34,13 +39,13 @@ def predict_sentence():
     with open(model_path, 'rb') as model_file:
         classifier = joblib.load(model_file)
 
-    predictions_test = classifier.predict(preprocess_sentence)
-    if predictions_test == 1:
+    predictions = classifier.predict(preprocess_sentence)
+    if predictions == 1:
         print_result = '<div class="result">\
                             Le sentiment de ce texte est \
                                 : <strong class="green">POSITIF</strong>\
                         </div>'
-    elif predictions_test == 0:
+    elif predictions == 0:
         print_result = '<div class="result">\
                             Le sentiment de ce texte est \
                                 : <strong class="red">NEGATIF</strong>\
